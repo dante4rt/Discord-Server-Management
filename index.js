3399cd3c7b388ebb02ba8122a33dac689c86fe73
Dante4rt <rxmxdhxni@gmail.com>
2024-03-03 11:15:44 +0700

    chore: add discord-command-manager

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord-simple-api');
 const schedule = require('node-schedule');
+const CommandManager = require('discord-command-manager');
 
 class ServerManagementBot {
   constructor(token) {
@@ -7,9 +8,20 @@ class ServerManagementBot {
       throw new Error('Please provide a bot token');
     }
     this.discord = new Discord(token);
+    this.commandManager = new CommandManager(this.discord);
+
+    this.registerCommands();
   }
 
-  // Schedule a message to be sent to a channel
+  registerCommands() {
+    this.commandManager.registerCommand('help', async (args, message) => {
+      await this.discord.sendMessageToChannel(
+        message.channel.id,
+        "Here's a list of available commands..."
+      );
+    });
+  }
+
   scheduleMessage(channelId, message, scheduleTime) {
     schedule.scheduleJob(scheduleTime, async () => {
       try {
@@ -21,73 +33,24 @@ class ServerManagementBot {
     });
   }
 
-  // Listen for a new member joining and auto-assign a role
-  async autoAssignRoleOnJoin(guildId, memberId, roleId) {
+  async displayServerStats(args, message) {
     try {
-      await this.discord.addRoleToMember(guildId, memberId, roleId);
-      console.log(
-        `Auto-assigned role ${roleId} to new member ${memberId} in guild ${guildId}`
-      );
-    } catch (error) {
-      console.error('Failed to auto-assign role on member join:', error);
-    }
-  }
-
-  // New Functionality: Auto-moderation for Banned Words
-  async deleteMessageWithBannedWords(channelId, bannedWords) {
-    const messages = await this.discord.getMessagesInChannel(channelId, 100);
-    messages.forEach(async (message) => {
-      if (bannedWords.some((word) => message.content.includes(word))) {
-        try {
-          await this.discord.deleteMessageInChannel(channelId, message.id);
-          console.log(
-            `Deleted message with banned word in channel ${channelId}`
-          );
-        } catch (error) {
-          console.error('Failed to delete message:', error);
-        }
-      }
-    });
-  }
-
-  // New Functionality: Welcome Message for New Members
-  async sendWelcomeMessage(guildId, memberId, welcomeMessage) {
-    try {
-      await this.discord.sendDirectMessage(memberId, {
-        content: welcomeMessage,
-      });
-      console.log(
-        `Welcome message sent to new member ${memberId} in guild ${guildId}`
-      );
-    } catch (error) {
-      console.error('Failed to send welcome message:', error);
-    }
-  }
-
-  // New Functionality: Goodbye Message for Leaving Members
-  async sendGoodbyeMessage(channelId, memberId, goodbyeMessage) {
-    try {
+      const guildId = message.guild.id;
+      const guildInfo = await this.discord.getGuild(guildId);
+      const responseMessage = `Server Name: ${guildInfo.name}\nTotal Members: ${guildInfo.member_count}`;
       await this.discord.sendMessageToChannel(
-        channelId,
-        goodbyeMessage.replace('{user}', memberId)
+        message.channel.id,
+        responseMessage
       );
-      console.log(
-        `Goodbye message posted in channel ${channelId} for member ${memberId}`
-      );
-    } catch (error) {
-      console.error('Failed to send goodbye message:', error);
-    }
-  }
-
-  // New Functionality: Fetch and Display Server Stats
-  async displayServerStats(guildId) {
-    try {
-      const guildInfo = await this.discord.getGuild(guildId); 
-      const message = `Server Name: ${guildInfo.name}\nTotal Members: ${guildInfo.member_count}`;
-      console.log(message);
     } catch (error) {
       console.error('Failed to fetch server stats:', error);
     }
+  }
+
+  listenForCommands() {
+    this.discord.on('message', (message) => {
+      this.commandManager.handleMessage(message);
+    });
   }
 }
 
